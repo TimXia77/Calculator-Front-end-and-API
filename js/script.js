a0b44605f4c84b4b02df2ce025f871ade9a837e2
timxia@cmail.carleton.com <timxia@cmail.carleton.ca>
2023-05-03 14:45:20 -0400

    Added some buttons that preform operations on values in the textfield
    These implementations are quick, inelegant, must be changed later

--- js/script.js
+++ js/script.js
@@ -1,20 +1,59 @@
-function calculate() {
 
-    console.log("Pressed Calculate")
+//INELEGANT IMPLEMENTAION - CHANGE LATER
+
+function add() {
+
+    console.log("Pressed Add")
+
+    let operand1 = Number(document.getElementById("operandField1").value)
+    let operand2 = Number(document.getElementById("operandField2").value)
+
+    let textDiv = document.getElementById("answer-area")
+    textDiv.innerHTML = "Answer: " + (operand1 + operand2)
+
+}
+
+function subtract() {
+
+    console.log("Pressed Subtract")
+
+    let operand1 = Number(document.getElementById("operandField1").value)
+    let operand2 = Number(document.getElementById("operandField2").value)
+
+    let textDiv = document.getElementById("answer-area")
+    textDiv.innerHTML = "Answer: " + operand1 - operand2
+
+}
+
+function multiply() {
+
+    console.log("Pressed Multiply")
+
+    let operand1 = Number(document.getElementById("operandField1").value)
+    let operand2 = Number(document.getElementById("operandField2").value)
+
+    let textDiv = document.getElementById("answer-area")
+    textDiv.innerHTML = "Answer: " + operand1 * operand2
+
+}
+
+function divide() {
+
+    console.log("Pressed Divide")
+
+    let operand1 = Number(document.getElementById("operandField1").value)
+    let operand2 = Number(document.getElementById("operandField2").value)
+
+    let textDiv = document.getElementById("answer-area")
+    textDiv.innerHTML = "Answer: " + operand1 / operand2
 
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('calculate_button').addEventListener('click', calculate)    
+
+    //operations
+    document.getElementById('add_button').addEventListener('click', add)    
+    document.getElementById('subtract_button').addEventListener('click', subtract)    
+    document.getElementById('multiply_button').addEventListener('click', multiply)    
+    document.getElementById('divide_button').addEventListener('click', divide)    
 })
-
-/*
-
-Send an email to the NSD
-
-follow up from, ticket closed, information needed
-
-copy paste information: 
-
-
-*/
