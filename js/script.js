ac889768ef0236cf72a30cda1394b80d5afd77fe
timxia@cmail.carleton.com <timxia@cmail.carleton.ca>
2023-05-03 13:11:43 -0400

    Wrote a test function for calculate button and updated html page(s) accordingly

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function calculate() {
+
+    console.log("Pressed Calculate")
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('calculate_button').addEventListener('click', calculate)    
+})
+
+/*
+
+Send an email to the NSD
+
+follow up from, ticket closed, information needed
+
+copy paste information: 
+
+
+*/
